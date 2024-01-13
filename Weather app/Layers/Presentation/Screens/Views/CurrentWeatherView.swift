c449a22c83a2cd48c9fb61f6d90f57d3d82be196
vladimir <vladimir.hodaryonok@idfeurasia.pro>
2024-01-13 15:27:06 +0300

    daily forecast carousel

---
+++ Weather app/Layers/Presentation/Screens/Views/CurrentWeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentWeather.swift
+//  Weather app
+//
+//  Created by Vladimir on 13.01.24.
+//
+
+import SwiftUI
+
+struct CurrentWeather: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrentWeather()
+}
---
+++ Weather app/Layers/Presentation/Screens/Views/DailiForecastList.swift
@@ -0,0 +1,18 @@
+//
+//  DailiForecastList.swift
+//  Weather app
+//
+//  Created by Vladimir on 13.01.24.
+//
+
+import SwiftUI
+
+struct DailiForecastList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DailiForecastList()
+}
